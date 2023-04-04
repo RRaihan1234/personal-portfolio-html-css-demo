523be16c661dfc0ad5ada805da1606871886d65e
RRaihan123 <ronyraihan141@gmail.com>
2023-04-04 14:19:57 +0600

    initial commit

---
+++ main.js
@@ -0,0 +1,253 @@
+// Header Scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+// nav hide 
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+
+
+
+document.querySelector(".btn1").addEventListener("click",function(){
+            /* document.querySelector("body").style.backgroundColor="gray"; */
+            let div1 = document.createElement("div");
+            
+            div1.style.background="rgb(0,0,0,0.5) url(./socialmedia.PNG) no-repeat center";
+            div1.style.backgroundSize="contain"
+           
+            div1.style.width = "100vw";
+            div1.style.height = "100vh";
+           
+            div1.style.cursor = "zoom-out";
+            div1.style.zIndex = "10000";
+            div1.style.position = "fixed";
+            div1.style.top = "0";
+            div1.style.left = "0";
+            document.querySelector("body").appendChild(div1)
+            div1.addEventListener("click",function(){
+                div1.remove();
+                
+             }) 
+            
+
+})
+document.querySelector(".btn2").addEventListener("click",function(){
+            /* document.querySelector("body").style.backgroundColor="gray"; */
+            let div1 = document.createElement("div");
+            
+            div1.style.background="rgb(0,0,0,0.5) url(./chatlatest.PNG) no-repeat center";
+            div1.style.backgroundSize="contain"
+           
+            div1.style.width = "100vw";
+            div1.style.height = "100vh";
+           
+            div1.style.cursor = "zoom-out";
+            div1.style.zIndex = "10000";
+            div1.style.position = "fixed";
+            div1.style.top = "0";
+            div1.style.left = "0";
+            document.querySelector("body").appendChild(div1)
+            div1.addEventListener("click",function(){
+                div1.remove();
+                
+             }) 
+            
+
+})
+document.querySelector(".btn3").addEventListener("click",function(){
+            /* document.querySelector("body").style.backgroundColor="gray"; */
+            let div1 = document.createElement("div");
+            
+            div1.style.background="rgb(0,0,0,0.5) url(./quiz1.PNG) no-repeat center";
+            div1.style.backgroundSize="contain"
+           
+            div1.style.width = "100vw";
+            div1.style.height = "100vh";
+           
+            div1.style.cursor = "zoom-out";
+            div1.style.zIndex = "10000";
+            div1.style.position = "fixed";
+            div1.style.top = "0";
+            div1.style.left = "0";
+            document.querySelector("body").appendChild(div1)
+            div1.addEventListener("click",function(){
+                div1.remove();
+                
+             }) 
+            
+
+})
+document.querySelector(".btn4").addEventListener("click",function(){
+            /* document.querySelector("body").style.backgroundColor="gray"; */
+            let div1 = document.createElement("div");
+            
+            div1.style.background="rgb(0,0,0,0.5) url(./crud.PNG) no-repeat center";
+            div1.style.backgroundSize="contain"
+           
+            div1.style.width = "100vw";
+            div1.style.height = "100vh";
+           
+            div1.style.cursor = "zoom-out";
+            div1.style.zIndex = "10000";
+            div1.style.position = "fixed";
+            div1.style.top = "0";
+            div1.style.left = "0";
+            document.querySelector("body").appendChild(div1)
+            div1.addEventListener("click",function(){
+                div1.remove();
+                
+             }) 
+            
+
+})
+document.querySelector(".btn5").addEventListener("click",function(){
+            /* document.querySelector("body").style.backgroundColor="gray"; */
+            let div1 = document.createElement("div");
+            
+            div1.style.background="rgb(0,0,0,0.5) url(./quiz.PNG) no-repeat center";
+            div1.style.backgroundSize="contain"
+           
+            div1.style.width = "100vw";
+            div1.style.height = "100vh";
+           
+            div1.style.cursor = "zoom-out";
+            div1.style.zIndex = "10000";
+            div1.style.position = "fixed";
+            div1.style.top = "0";
+            div1.style.left = "0";
+            document.querySelector("body").appendChild(div1)
+            div1.addEventListener("click",function(){
+                div1.remove();
+                
+             }) 
+            
+
+})
+document.querySelector(".btn6").addEventListener("click",function(){
+            /* document.querySelector("body").style.backgroundColor="gray"; */
+            let div1 = document.createElement("div");
+            
+            div1.style.background="rgb(0,0,0,0.5) url(./country.PNG) no-repeat center";
+            div1.style.backgroundSize="contain"
+           
+            div1.style.width = "100vw";
+            div1.style.height = "100vh";
+           
+            div1.style.cursor = "zoom-out";
+            div1.style.zIndex = "10000";
+            div1.style.position = "fixed";
+            div1.style.top = "0";
+            div1.style.left = "0";
+            document.querySelector("body").appendChild(div1)
+            div1.addEventListener("click",function(){
+                div1.remove();
+                
+             }) 
+            
+
+})
+document.querySelector(".btn7").addEventListener("click",function(){
+            /* document.querySelector("body").style.backgroundColor="gray"; */
+            let div1 = document.createElement("div");
+            
+            div1.style.background="rgb(0,0,0,0.5) url(./bookRedux.PNG) no-repeat center";
+            div1.style.backgroundSize="contain"
+           
+            div1.style.width = "100vw";
+            div1.style.height = "100vh";
+           
+            div1.style.cursor = "zoom-out";
+            div1.style.zIndex = "10000";
+            div1.style.position = "fixed";
+            div1.style.top = "0";
+            div1.style.left = "0";
+            document.querySelector("body").appendChild(div1)
+            div1.addEventListener("click",function(){
+                div1.remove();
+                
+             }) 
+            
+
+})
+document.querySelector(".btn8").addEventListener("click",function(){
+            /* document.querySelector("body").style.backgroundColor="gray"; */
+            let div1 = document.createElement("div");
+            
+            div1.style.background="rgb(0,0,0,0.5) url(./emp_info_storage.PNG) no-repeat center";
+            div1.style.backgroundSize="contain"
+           
+            div1.style.width = "100vw";
+            div1.style.height = "100vh";
+           
+            div1.style.cursor = "zoom-out";
+            div1.style.zIndex = "10000";
+            div1.style.position = "fixed";
+            div1.style.top = "0";
+            div1.style.left = "0";
+            document.querySelector("body").appendChild(div1)
+            div1.addEventListener("click",function(){
+                div1.remove();
+                
+             }) 
+            
+
+})
+document.querySelector(".btn9").addEventListener("click",function(){
+            /* document.querySelector("body").style.backgroundColor="gray"; */
+            let div1 = document.createElement("div");
+            
+            div1.style.background="rgb(0,0,0,0.5) url(./html_css1.PNG) no-repeat center";
+            div1.style.backgroundSize="contain"
+           
+            div1.style.width = "100vw";
+            div1.style.height = "100vh";
+           
+            div1.style.cursor = "zoom-out";
+            div1.style.zIndex = "10000";
+            div1.style.position = "fixed";
+            div1.style.top = "0";
+            div1.style.left = "0";
+            document.querySelector("body").appendChild(div1)
+            div1.addEventListener("click",function(){
+                div1.remove();
+                
+             }) 
+            
+
+})
+document.querySelector(".btn10").addEventListener("click",function(){
+            /* document.querySelector("body").style.backgroundColor="gray"; */
+            let div1 = document.createElement("div");
+            
+            div1.style.background="rgb(0,0,0,0.5) url(./html_css2.PNG) no-repeat center";
+            div1.style.backgroundSize="contain"
+           
+            div1.style.width = "100vw";
+            div1.style.height = "100vh";
+           
+            div1.style.cursor = "zoom-out";
+            div1.style.zIndex = "10000";
+            div1.style.position = "fixed";
+            div1.style.top = "0";
+            div1.style.left = "0";
+            document.querySelector("body").appendChild(div1)
+            div1.addEventListener("click",function(){
+                div1.remove();
+                
+             }) 
+            
+
+})
+ 
